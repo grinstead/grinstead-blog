bfef528f18d49deddf4e0c1d5962821815d0061c
Joe Grinstead <npm@mailforce.net>
2023-09-02 15:26:16 -0400

    Initial commit
    Created with npm init vite

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = createSignal(0);
+
+  return (
+    <div class="paper">
+      <div class="header">Joe Grinstead's Code Blog</div>
+      <div class="body">
+        <p>Concept concept concept</p>
+      </div>
+      <div class="footer">Blog Post Footer</div>
+    </div>
+  );
+}
+
+export default App;
